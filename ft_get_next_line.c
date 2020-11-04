0bbe2783da77ee1550b406c4c3614fd511f14ceb
Vitaly <vitaly_0818@mail.ru>
2020-11-04 23:18:55 +0300

    1

---
+++ ft_get_next_line.c
@@ -0,0 +1,6 @@
+#include "get_next_line.h"
+
+int	ft_get_next_line(int fd, char **line)
+{
+
+}
---
+++ ft_get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_N
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_get_next_line(int fd, char **line);
+
+#endif
