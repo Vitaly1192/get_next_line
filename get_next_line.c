feac4833fd5afef50fe456672e880ba8fb5cbf71
Vitaly <vitaly_0818@mail.ru>
2020-11-16 01:03:27 +0300

    1.1

--- get_next_line.c
+++ get_next_line.c
@@ -18,7 +18,7 @@
 ** -1 –– при любой ошибки: fd –– испорчиный, не смогли аллоцировать память и тд.
 */
 
-int		ft_clear(char **str, char *str2)
+int		ft_error(char **str, char *str2)
 {
 	free(*str);
 	*str = NULL;
@@ -26,7 +26,7 @@ int		ft_clear(char **str, char *str2)
 	return (-1);
 }
 
-char	*ft_strjoin_to_endline_and_free(char **s1, char *s2)
+char	*ft_strjoin_to_new_line_and_free(char **s1, char *s2)
 {
 	char	*str;
 	size_t	k;
@@ -50,7 +50,9 @@ char	*ft_strjoin_to_endline_and_free(char **s1, char *s2)
 		*str++ = *s2++;
 		k++;
 	}
-	ft_clear(s1, NULL);
+	// ft_error(s1, NULL);
+	free(*s1);
+	*s1 = NULL;
 	return (str - k);
 }
 
@@ -63,23 +65,23 @@ int		check_tail(char **line, char **s_tail, char **find_end, char **array)
 	{
 		*(*find_end) = '\0';
 		if (!(*line = ft_strdup(*s_tail)))
-			return (ft_clear(s_tail, NULL));
+			return (ft_error(s_tail, NULL));
 		cur_ptr = *s_tail;
-		if (!(*s_tail = ft_strdup(*find_end + 1)) && ft_clear(&cur_ptr, NULL))
-			return (-1);
-		ft_clear(&cur_ptr, NULL);
+		if (!(*s_tail = ft_strdup(*find_end + 1)) && ft_error(&cur_ptr, NULL))
+			return (-1); // перенести очищение сюда
+		ft_error(&cur_ptr, NULL);
 		return (1);
 	}
 	else if (*s_tail != NULL)
 	{
 		if ((*line = ft_strdup(*s_tail)) == NULL)
-			return (ft_clear(s_tail, NULL));
-		ft_clear(s_tail, NULL);
+			return (ft_error(s_tail, NULL));
+		ft_error(s_tail, NULL);
 	}
 	else
-		*line = (char *)ft_calloc(1, 1);
+		*line = ft_strdup("");
 	if (*line == NULL || !(*array = (char *)malloc(BUFFER_SIZE + 1)))
-		return (ft_clear(s_tail, NULL));
+		return (ft_error(s_tail, NULL));
 	return (2);
 }
 
@@ -90,7 +92,7 @@ int		set_line(char **array, char **s_tail, char **find_end)
 		*(*find_end) = '\0';
 		free(*s_tail);
 		if (!(*s_tail = ft_strdup(*find_end + 1)))
-			return (ft_clear(s_tail, *array));
+			return (ft_error(s_tail, *array));
 	}
 	return (1);
 }
@@ -102,9 +104,9 @@ int		get_next_line(int fd, char **line)
 	char			*find_end;
 	int				r;
 
-	find_end = NULL;
-	if (fd < 0 || line == NULL || read(fd, find_end, 0) < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || line == NULL || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
 		return (-1);
+	find_end = NULL;
 	if ((r = check_tail(line, &s_tail, &find_end, &array)) == -1)
 		return (-1);
 	else if (r == 1)
@@ -114,11 +116,11 @@ int		get_next_line(int fd, char **line)
 		array[r] = '\0';
 		if (set_line(&array, &s_tail, &find_end) == -1)
 			return (-1);
-		if ((*line = ft_strjoin_to_endline_and_free(line, array)) == NULL)
-			return (ft_clear(&s_tail, array));
+		if ((*line = ft_strjoin_to_new_line_and_free(line, array)) == NULL)
+			return (ft_error(&s_tail, array));
 	}
-	if (r < 0)
-		return (ft_clear(&s_tail, array));
 	free(array);
+	if (r == -1)
+		return (ft_error(&s_tail, NULL));
 	return (r == 0 ? 0 : 1);
 }
